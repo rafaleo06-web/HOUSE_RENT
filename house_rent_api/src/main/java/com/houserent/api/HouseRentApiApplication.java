9fb1d29c2b8f29eb94d4d49755870a3fdb25d0b0
Rafael <rafa_061@hotmail.com>
2021-10-13 12:33:21 -0500

    inicio de proyecto

---
+++ house_rent_api/src/main/java/com/houserent/api/HouseRentApiApplication.java
@@ -0,0 +1,13 @@
+package com.houserent.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouseRentApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HouseRentApiApplication.class, args);
+    }
+
+}
---
+++ house_rent_api/src/test/java/com/houserent/api/HouseRentApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.houserent.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HouseRentApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
